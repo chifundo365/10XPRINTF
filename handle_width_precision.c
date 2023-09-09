4045054dad7b08641bb5f9868175c600f736b998
chifundo bizweck <chifundobiziweck@gmail.com>
2023-09-09 10:12:41 +0000

    initial precision commit

---
+++ handle_width_precision.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+void width_options(const char *format, va_list list, Width_Opt *options)
+{
+	int width = 0;
+	int zero_flag = 0;
+	int format_position = 0;
+
+	if (*format == '0')
+	{
+		zero_flag = 1;
+		format++;
+		format_position++;
+	}
+
+
+	if (*format >= '0' && *format <= '9')
+	{
+
+		width = *format - '0';
+		format++;
+		format_position++;
+
+		while (*format >= '0' && *format <= '9')
+		{
+			width = width * 10 + (*format - '0');
+			format++;
+			format_position++;
+		}
+	}
+
+
+
+
+	if (*format  == '*')
+	{
+		width = va_arg(list, int);
+		format_position++;
+	}
+
+	options->width  = width;
+	options->zero_flag = zero_flag;
+	options->format_position = format_position;
+
+}
+
+
